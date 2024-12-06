85b2febccd87e92dd4932bd64855d9970e4334fb
conple21 <conple21@gmail.com>
2024-12-06 15:51:07 +0900

    1206 (1) Creating a 'DedicatedServers' Module

---
+++ Source/DedicatedServers/DedicatedServers.Build.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+
+public class DedicatedServers : ModuleRules
+{
+	public DedicatedServers(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine"});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+	}
+}
