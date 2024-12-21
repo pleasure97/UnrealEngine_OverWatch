6158063850aedda5bf1e1f9f81213a5162569c44
conple21 <conple21@gmail.com>
2024-12-21 12:42:45 +0900

    1221 (2) Retrieve Match Stats Endpoint

--- Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
+++ Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
@@ -11,14 +11,15 @@ namespace DedicatedServersTags
 
 	namespace PortalAPI
 	{
-		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignUp, "DedicatedServersTags.PortalAPI.SignUp", "Creates a new user in the Portal API");
-		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ConfirmSignUp, "DedicatedServersTags.PortalAPI.ConfirmSignUp", "Confirms the new user in Portal API");
-		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignIn, "DedicatedServersTags.PortalAPI.SignIn", "Retrieves Access Token, ID Token, and Refresh Token in Portal API");
-		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignOut, "DedicatedServersTags.PortalAPI.SignOut", "Signs user out and invalidates tokens in Poratl API");
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignUp, "DedicatedServersTags.PortalAPI.SignUp", "Creates a new user in the Portal API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ConfirmSignUp, "DedicatedServersTags.PortalAPI.ConfirmSignUp", "Confirms the new user in Portal API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignIn, "DedicatedServersTags.PortalAPI.SignIn", "Retrieves Access Token, ID Token, and Refresh Token in Portal API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignOut, "DedicatedServersTags.PortalAPI.SignOut", "Signs user out and invalidates tokens in Poratl API")
 	}
 
 	namespace GameStatsAPI
 	{
 		UE_DEFINE_GAMEPLAY_TAG_COMMENT(RecordMatchStats, "DedicatedServersTags.GameStatsAPI.RecordMatchStats", "Records the stats of a match in Game Stats API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(RetrieveMatchStats, "DedicatedServersTags.GameStatsAPI.RetrieveMatchStats", "Gets the match stats from the Players table in Game Stats API")
 	}
 }
