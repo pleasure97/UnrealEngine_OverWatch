7ffd084fb7d1c95797564cc6e30f1b702a74a9f1
conple21 <conple21@gmail.com>
2025-01-05 17:06:47 +0900

    0105 (4) Add Player State

---
+++ Source/FPSTemplate/Private/Player/OWPlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/OWPlayerState.h"
+
+AOWPlayerState::AOWPlayerState()
+{
+	NetUpdateFrequency = 100.f; 
+}
---
+++ Source/FPSTemplate/Public/Player/OWPlayerState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/DS_MatchPlayerState.h"
+#include "OWPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API AOWPlayerState : public ADS_MatchPlayerState
+{
+	GENERATED_BODY()
+	
+public:
+	AOWPlayerState(); 
+	
+};
