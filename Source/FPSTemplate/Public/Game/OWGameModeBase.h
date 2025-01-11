b06bfddcfe17445df62b8747e5cb224907c49086
conple21 <conple21@gmail.com>
2025-01-11 17:44:13 +0900

    0111 (4) Change StartupMap and Create Blueprints of GameMode, Player State, and HUD

---
+++ Source/FPSTemplate/Public/Game/OWGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/DS_GameModeBase.h"
+#include "OWGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API AOWGameModeBase : public ADS_GameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
