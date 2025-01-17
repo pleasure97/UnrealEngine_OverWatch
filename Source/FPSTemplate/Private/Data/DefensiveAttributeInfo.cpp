d0b3299ba1d08cff9b627c7efe76724f1e3f60d1
conple21 <conple21@gmail.com>
2025-01-17 17:10:18 +0900

    0117 (2) Make Data Asset for Defensive Attribute

---
+++ Source/FPSTemplate/Private/Data/DefensiveAttributeInfo.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/DefensiveAttributeInfo.h"
+
+FAttributeDefensiveInfo UDefensiveAttributeInfo::FindDefensiveAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FAttributeDefensiveInfo& Info : DefensiveAttributeInfo)
+	{
+		if (Info.DefensiveAttributeTag == AttributeTag)
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find info for Ability Tag [%s] on AbilityInfo [%s]"), *AttributeTag.ToString(), *GetNameSafe(this));
+	}
+
+	return FAttributeDefensiveInfo();
+}
---
+++ Source/FPSTemplate/Public/Data/DefensiveAttributeInfo.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "DefensiveAttributeInfo.generated.h"
+
+class UTexture2D; 
+
+USTRUCT(BlueprintType)
+struct FAttributeDefensiveInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag DefensiveAttributeTag = FGameplayTag(); 
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FLinearColor Tint_Fill = FLinearColor(0.f, 0.f, 0.f, 1.f);
+};
+
+UCLASS()
+class FPSTEMPLATE_API UDefensiveAttributeInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attribute Information")
+	TArray<FAttributeDefensiveInfo> DefensiveAttributeInfo; 
+
+	FAttributeDefensiveInfo FindDefensiveAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound = false) const; 	
+};
