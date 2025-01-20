1d89e53b4061302c3cd7935d7669314d0e5fe107
conple21 <conple21@gmail.com>
2025-01-20 20:47:50 +0900

    0120 (5) Initialize Damage Gameplay Effect Execution Calculation

---
+++ Source/FPSTemplate/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "ExecCalc_Damage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API UExecCalc_Damage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
