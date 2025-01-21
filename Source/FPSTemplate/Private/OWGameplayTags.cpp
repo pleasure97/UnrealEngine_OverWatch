645e4d3b60a77898660929201b19a4c1540e8047
conple21 <conple21@gmail.com>
2025-01-21 15:58:23 +0900

    0121 (5) Add Move, Attack, and Skill GameplayTags to GameplayTags Singleton Class

--- Source/FPSTemplate/Private/OWGameplayTags.cpp
+++ Source/FPSTemplate/Private/OWGameplayTags.cpp
@@ -38,7 +38,64 @@ void FOWGameplayTags::InitializeNativeGameplayTags()
 		FName("Attributes.Defense.OverHealth"),
 		FString("Over Health"));
 
+	/* Input Attributes - Move */
+
+	GameplayTags.InputTag_Move_Forward = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Move.Forward"),
+		FString("InputTag of Move Forward"));
+
+	GameplayTags.InputTag_Move_Left = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Move.Left"),
+		FString("InputTag of Move Left"));
+
+	GameplayTags.InputTag_Move_Backward = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Move.Backward"),
+		FString("InputTag of Move Backward"));
+
+	GameplayTags.InputTag_Move_Right = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Move.Right"),
+		FString("InputTag of Move Right"));
+
+	GameplayTags.InputTag_Crouch = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Crouch"),
+		FString("InputTag of Crouch"));
+
+	GameplayTags.InputTag_Jump = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Jump"),
+		FString("InputTag of Jump"));
+
+	/* Input Attributes - Weapon & Skill */
+
+	GameplayTags.InputTag_Skill_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Skill.1"),
+		FString("InputTag of Skill 1"));
+
+	GameplayTags.InputTag_Skill_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Skill.2"),
+		FString("InputTag of Skill 2"));
+
+	GameplayTags.InputTag_Ultimate = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Ultimate"),
+		FString("InputTag of Ultimate"));
+
+	GameplayTags.InputTag_QuickAttack = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.QuickAttack"),
+		FString("InputTag of QuickAttack"));
+
+	GameplayTags.InputTag_Reload = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Reload"),
+		FString("InputTag of Reload"));
+
+	GameplayTags.InputTag_DefaultFire = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.DeafultFire"),
+		FString("InputTag of DefaultFire"));
+
+	GameplayTags.InputTag_SecondaryFire = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.SecondaryFire"),
+		FString("InputTag of SecondaryFire"));
+
 	/* Player Input Attributes */
+
 	GameplayTags.Player_Input_Pressed = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Player.Input.Pressed"),
 		FString("Player Input Pressed")); 
--- Source/FPSTemplate/Public/OWGameplayTags.h
+++ Source/FPSTemplate/Public/OWGameplayTags.h
@@ -26,6 +26,23 @@ public:
 	FGameplayTag Attributes_Defense_TempShield;
 	FGameplayTag Attributes_Defense_OverHealth;
 
+	/* Input Attributes - Move */
+	FGameplayTag InputTag_Move_Forward;
+	FGameplayTag InputTag_Move_Left; 
+	FGameplayTag InputTag_Move_Backward;
+	FGameplayTag InputTag_Move_Right; 
+	FGameplayTag InputTag_Crouch;
+	FGameplayTag InputTag_Jump; 
+
+	/* Input Attributes - Weapon & Skill */
+	FGameplayTag InputTag_Skill_1; 
+	FGameplayTag InputTag_Skill_2; 
+	FGameplayTag InputTag_Ultimate; 
+	FGameplayTag InputTag_QuickAttack; 
+	FGameplayTag InputTag_Reload; 
+	FGameplayTag InputTag_DefaultFire; 
+	FGameplayTag InputTag_SecondaryFire; 
+
 	/* Player Input Attributes */
 	FGameplayTag Player_Input_Pressed; 
 	FGameplayTag Player_Input_Held; 
