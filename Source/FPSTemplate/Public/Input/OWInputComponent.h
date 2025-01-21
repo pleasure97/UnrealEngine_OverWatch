a7b27190b35d81ed8d9e7feb4eea65003e3915cd
conple21 <conple21@gmail.com>
2025-01-21 14:26:23 +0900

    0121 (2) Initialize Enhanced Input Component with Binding GameplayTags

---
+++ Source/FPSTemplate/Public/Input/OWInputComponent.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "OWInputConfig.h"
+#include "OWInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API UOWInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+public:
+	template<class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+	void BindAbilityActions(const UOWInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc); 	
+};
+
+template<class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+void UOWInputComponent::BindAbilityActions(const UOWInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc)
+{
+	check(InputConfig); 
+
+	for (const FOWInputAction& Action : InputConfig->AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag.IsValid())
+		{
+			if (PressedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Started, Object, PressedFunc, Action.InputTag); 
+			}
+			if (ReleasedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, Action.InputTag); 
+			}
+			if (HeldFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Triggered, Object, HeldFunc, Action.InputTag); 
+			}
+		}
+	}
+}
