2482016c508e62443833027791e6ca3156eded53
conple21 <conple21@gmail.com>
2025-01-22 15:33:23 +0900

    0122 (1) Update CauseDamage() of Damage Gameplay Ability

--- Source/FPSTemplate/Private/AbilitySystem/Abilities/OWDamageGameplayAbility.cpp
+++ Source/FPSTemplate/Private/AbilitySystem/Abilities/OWDamageGameplayAbility.cpp
@@ -2,7 +2,16 @@
 
 
 #include "AbilitySystem/Abilities/OWDamageGameplayAbility.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
 
+void UOWDamageGameplayAbility::CauseDamage(AActor* TargetActor)
+{
+	FGameplayEffectSpecHandle DamageSpecHandle = MakeOutgoingGameplayEffectSpec(DamageEffectClass, 1.f); 
 
+	const float ScaledDamage = Damage.GetValueAtLevel(GetAbilityLevel()); 
+	UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(DamageSpecHandle, DamageType, ScaledDamage); 
 
-
+	GetAbilitySystemComponentFromActorInfo()->ApplyGameplayEffectSpecToTarget(*DamageSpecHandle.Data.Get(), 
+		UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor)); 
+}
--- Source/FPSTemplate/Public/AbilitySystem/Abilities/OWDamageGameplayAbility.h
+++ Source/FPSTemplate/Public/AbilitySystem/Abilities/OWDamageGameplayAbility.h
@@ -14,7 +14,37 @@ class FPSTEMPLATE_API UOWDamageGameplayAbility : public UOWGameplayAbility
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+public:
+	void CauseDamage(AActor* TargetActor); 
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> DamageEffectClass; 
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	FGameplayTag DamageType; 
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	FScalableFloat Damage; 
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffChance = 0.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffDamage = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffFrequency = 1.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffDuration = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DeathImpulseMagnitude = 1000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float KnockbackForceMagnitude = 1000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float KnockbackChance = 0.f;
 };
