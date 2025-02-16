959a2a788502e61441bb57d681984ab656c86c4a
conple21 <conple21@gmail.com>
2025-02-16 13:58:00 +0900

    0216 (2) Add Illiari's Ability, Cost, and Cooldown
    - Damage Gameplay Ability
      > GA_SolarRifle
      > GA_HealingRay
    - Cost
      > GE_Cost_SolarRifle
      > GE_Cost_HealingRay
    - Cooldown
      > GE_Cooldown_SolarRifle
      > GE_Cooldown_HealingRay

---
+++ Source/FPSTemplate/Public/AbilitySystem/Abilities/Support/Illiari/HealingRay.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/OWDamageGameplayAbility.h"
+#include "HealingRay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API UHealingRay : public UOWDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
