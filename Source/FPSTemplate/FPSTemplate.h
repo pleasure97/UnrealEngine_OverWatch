3ded775b76fa30ebcf84c79e82977c129813ba1b
conple21 <conple21@gmail.com>
2025-02-23 20:21:06 +0900

    0223 (2) Add Custom Depth Macro

--- Source/FPSTemplate/FPSTemplate.h
+++ Source/FPSTemplate/FPSTemplate.h
@@ -5,3 +5,7 @@
 #include "CoreMinimal.h"
 
 #define ECC_Weapon ECollisionChannel::ECC_GameTraceChannel1
+
+#define CUSTOM_DEPTH_RED 250 
+#define CUSTOM_DEPTH_BLUE 251
+#define CUSTOM_DEPTH_TAN 252 
