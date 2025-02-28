b12f2485d3d7218d8080d3ef6f4bb52e3163e29e
conple21 <conple21@gmail.com>
2025-02-28 15:17:22 +0900

    0228 (4) Fix BroadcastInitialValues() in Overlay Widget Controller
    - BroadcastInitialValues()
      > Broadcast Gameplay Attribute from Attribute Changed Delegate to Initialize Widgets

--- Source/FPSTemplate/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/FPSTemplate/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -11,47 +11,54 @@ void UOverlayWidgetController::BroadcastInitialValues()
 {
 	check(HeroInfo); 
 
-	BroadcastHeroInfo(); 
+	BroadcastHeroInfo();
 
-	for (TPair<FGameplayTag, TStaticFuncPtr<FGameplayAttribute()>>& Pair : GetOW_AS()->TagsToAttributes)
-	{
-		FOnAttributeChangedSignature& AttributeDelegate = GetDelegateForTag(Pair.Key); 
-		BindAttributeChange(AbilitySystemComponent, Pair.Key, Pair.Value(), AttributeDelegate); 
-	}
+	OnMaxHealthChanged.Broadcast(GetOW_AS()->GetMaxHealth());
+	OnMaxArmorChanged.Broadcast(GetOW_AS()->GetMaxArmor());
+	OnMaxShieldChanged.Broadcast(GetOW_AS()->GetMaxShield());
+	OnHealthChanged.Broadcast(GetOW_AS()->GetHealth());
+	OnArmorChanged.Broadcast(GetOW_AS()->GetArmor());
+	OnShieldChanged.Broadcast(GetOW_AS()->GetShield());
+	OnTempArmorChanged.Broadcast(GetOW_AS()->GetTempArmor());
+	OnTempShieldChanged.Broadcast(GetOW_AS()->GetTempShield());
+	OnOverHealthChanged.Broadcast(GetOW_AS()->GetOverHealth());
+	OnUltimateGaugeChanged.Broadcast(GetOW_AS()->GetUltimateGauge()); 
+	OnMaxUltimateGaugeChanged.Broadcast(GetOW_AS()->GetMaxUltimateGauge()); 
+	OnNumCurrentBulletsChanged.Broadcast(GetOW_AS()->GetNumCurrentBullets()); 
+	OnNumMaxBulletsChanged.Broadcast(GetOW_AS()->GetNumMaxBullets()); 
 }
 
 void UOverlayWidgetController::BindCallbacksToDependencies()
 {
-	/* Ability Delegate */
-	if (GetOW_ASC() && GetOW_ASC()->bDefaultAbilitiesGiven)
+	if (GetOW_ASC())
 	{
-		BroadcastHeroInfo(); 
+		BroadcastHeroInfo();
 	}
 
 	for (TPair<FGameplayTag, TStaticFuncPtr<FGameplayAttribute()>>& Pair : GetOW_AS()->TagsToAttributes)
 	{
-		FOnAttributeChangedSignature& AttributeDelegate = GetDelegateForTag(Pair.Key);
-		BindAttributeChange(AbilitySystemComponent, Pair.Key, Pair.Value(), AttributeDelegate);
+		FOnAttributeChangedSignature* AttributeDelegate = GetDelegateForTag(Pair.Key);
+		BindAttributeChange(AbilitySystemComponent, Pair.Key, Pair.Value(), *AttributeDelegate);
 	}
 }
 
-FOnAttributeChangedSignature& UOverlayWidgetController::GetDelegateForTag(const FGameplayTag& Tag)
+FOnAttributeChangedSignature* UOverlayWidgetController::GetDelegateForTag(const FGameplayTag& Tag)
 {
-	if (Tag == FOWGameplayTags::Get().Attributes_Defense_Health) return OnHealthChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Defense_MaxHealth) return OnMaxHealthChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Defense_Armor) return OnArmorChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Defense_MaxArmor) return OnMaxArmorChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Defense_Shield) return OnShieldChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Defense_MaxShield) return OnMaxShieldChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Defense_TempArmor) return OnTempArmorChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Defense_TempShield) return OnTempShieldChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Defense_OverHealth) return OnOverHealthChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Skill_UltimateGauge) return OnUltimateGaugeChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Skill_MaxUltimateGauge) return OnUltimateGaugeChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Skill_NumCurrentBullets) return OnNumCurrentBulletsChanged;
-	if (Tag == FOWGameplayTags::Get().Attributes_Skill_NumMaxBullets) return OnNumMaxBulletsChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Defense_Health) return &OnHealthChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Defense_MaxHealth) return &OnMaxHealthChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Defense_Armor) return &OnArmorChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Defense_MaxArmor) return &OnMaxArmorChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Defense_Shield) return &OnShieldChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Defense_MaxShield) return &OnMaxShieldChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Defense_TempArmor) return &OnTempArmorChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Defense_TempShield) return &OnTempShieldChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Defense_OverHealth) return &OnOverHealthChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Skill_UltimateGauge) return &OnUltimateGaugeChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Skill_MaxUltimateGauge) return &OnMaxUltimateGaugeChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Skill_NumCurrentBullets) return &OnNumCurrentBulletsChanged;
+	if (Tag == FOWGameplayTags::Get().Attributes_Skill_NumMaxBullets) return &OnNumMaxBulletsChanged;
 
-	return OnHealthChanged; 
+	return nullptr; 
 }
 
 void UOverlayWidgetController::BindAttributeChange(UAbilitySystemComponent* ASC, const FGameplayTag& Tag, const FGameplayAttribute& Attribute, FOnAttributeChangedSignature& Delegate)
@@ -59,7 +66,7 @@ void UOverlayWidgetController::BindAttributeChange(UAbilitySystemComponent* ASC,
 	ASC->GetGameplayAttributeValueChangeDelegate(Attribute).AddLambda(
 		[&](const FOnAttributeChangeData& Data)
 		{
-			Delegate.Broadcast(Tag, Data.NewValue);
+			Delegate.Broadcast(Data.NewValue);
 		}
 	);
 }
