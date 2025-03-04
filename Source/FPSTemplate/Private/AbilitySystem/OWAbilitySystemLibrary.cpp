d735ef94ffa509700dcf69190524fccba4bc64bd
conple21 <conple21@gmail.com>
2025-03-04 17:32:08 +0900

    0304 (6) Update Functions in Ability System Blueprint Library
    - Effect Context Getter and Setter
      > Using EffectContextHandle, Get and Set Effect Context Parameters
      > CriticalHit
      > Debuff Damage, Duration, Frequency
      > Damage Type
      > Death Impulse, Knockback Force
      > Radial Damage, Radius, Origin
    - Gameplay Mechanics : Apply Damage Effect()
      > Make Effect Context
      > Set Source Object
      > Set Effect Context Custom Parameters
      > Make Outgoing Spec
      > Assign Tag Set By Caller Magnitude
      > Apply Gameplay Effect Spec to Target
    - Gameplay Mechanics : Set Knockback Direction, Set DeathImpulse Direction
      > Normalize Direction
      > Multiply Magintude or Damage Effect Params' Magnitude

--- Source/FPSTemplate/Private/AbilitySystem/OWAbilitySystemLibrary.cpp
+++ Source/FPSTemplate/Private/AbilitySystem/OWAbilitySystemLibrary.cpp
@@ -9,8 +9,11 @@
 #include "AbilitySystemComponent.h"
 #include "AbilitySystem/Data/HeroInfo.h"
 #include "Game/OWGameModeBase.h"
-#include "Player/OWPlayerState.h"
+#include "OWGameplayTags.h"
+#include "OWAbilityTypes.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
+/* Widget Controller */
 bool UOWAbilitySystemLibrary::MakeWidgetControllerParams(const UObject* WorldContextObject, FWidgetControllerParams& OutWCParams, AOWHUD*& OutOWHUD)
 {
 	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
@@ -45,6 +48,7 @@ UOverlayWidgetController* UOWAbilitySystemLibrary::GetOverlayWidgetController(co
 	return nullptr;
 }
 
+/* Hero Info Defaults */
 void UOWAbilitySystemLibrary::InitializeDefaultAttributes(const UObject* WorldContextObject, EHeroName HeroName, UAbilitySystemComponent* ASC, float Level)
 {
 	AActor* AvatarActor = ASC->GetAvatarActor(); 
@@ -96,4 +100,259 @@ EHeroName UOWAbilitySystemLibrary::GetHeroName(const UObject* WorldContextObject
 
 	const AOWPlayerState* OWPlayerState = Cast<AOWPlayerState>(UGameplayStatics::GetPlayerState(WorldContextObject, 0));
 	return OWPlayerState ? OWPlayerState->SelectedHeroName : EHeroName::None;
+}
+
+/* Effect Context Getter */
+bool UOWAbilitySystemLibrary::IsCriticalHit(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->IsCriticalHit();
+	}
+	return false;
+}
+
+bool UOWAbilitySystemLibrary::IsSuccessfulDebuff(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->IsSuccessfulDebuff();
+	}
+	return false;
+}
+
+float UOWAbilitySystemLibrary::GetDebuffDamage(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->GetDebuffDamage();
+	}
+	return 0.0f;
+}
+
+float UOWAbilitySystemLibrary::GetDebuffDuration(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->GetDebuffDuration();
+	}
+	return 0.0f;
+}
+
+float UOWAbilitySystemLibrary::GetDebuffFrequency(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->GetDebuffFrequency();
+	}
+	return 0.0f;
+}
+
+FGameplayTag UOWAbilitySystemLibrary::GetDamageType(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		if (OWEffectContext->GetDamageType().IsValid())
+		{
+			return *OWEffectContext->GetDamageType(); 
+		}
+	}
+	return FGameplayTag();
+}
+
+FVector UOWAbilitySystemLibrary::GetDeathImpulse(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->GetDeathImpulse();
+	}
+	return FVector();
+}
+
+FVector UOWAbilitySystemLibrary::GetKnockbackForce(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->GetKnockbackForce();
+	}
+	return FVector::ZeroVector;
+}
+
+bool UOWAbilitySystemLibrary::bIsRadialDamage(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->IsRadialDamage();
+	}
+	return false;
+}
+
+float UOWAbilitySystemLibrary::GetRadialDamageRadius(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->GetRadialDamageRadius();
+	}
+	return 0.0f;
+}
+
+FVector UOWAbilitySystemLibrary::GetRadialDamageOrigin(const FGameplayEffectContextHandle& EffectContextHandle)
+{
+	if (const FOWGameplayEffectContext* OWEffectContext = static_cast<const FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		return OWEffectContext->GetRadialDamageOrigin();
+	}
+	return FVector::ZeroVector;
+}
+
+/* Effect Context Setter */
+void UOWAbilitySystemLibrary::SetIsCriticalHit(FGameplayEffectContextHandle& EffectContextHandle, bool bInIsCriticalHit)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetIsCriticalHit(bInIsCriticalHit); 
+	}	
+}
+
+void UOWAbilitySystemLibrary::SetIsSuccessfulDebuff(FGameplayEffectContextHandle& EffectContextHandle, bool bInIsSuccessfulDebuff)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetIsSuccessfulDebuff(bInIsSuccessfulDebuff);
+	}
+}
+
+void UOWAbilitySystemLibrary::SetDebuffDamage(FGameplayEffectContextHandle& EffectContextHandle, float InDebuffDamage)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetDebuffDamage(InDebuffDamage);
+	}
+}
+
+void UOWAbilitySystemLibrary::SetDebuffDuration(FGameplayEffectContextHandle& EffectContextHandle, float InDebuffDuration)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetDebuffDuration(InDebuffDuration); 
+	}
+}
+
+void UOWAbilitySystemLibrary::SetDebuffFrequency(FGameplayEffectContextHandle& EffectContextHandle, float InDebuffFrequency)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetDebuffFrequency(InDebuffFrequency); 
+	}
+}
+
+void UOWAbilitySystemLibrary::SetDamageType(FGameplayEffectContextHandle& EffectContextHandle, const FGameplayTag& InDamageType)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		const TSharedPtr<FGameplayTag> DamageType = MakeShared<FGameplayTag>(InDamageType); 
+		OWEffectContext->SetDamageType(DamageType); 
+	}
+}
+
+void UOWAbilitySystemLibrary::SetDeathImpulse(FGameplayEffectContextHandle& EffectContextHandle, const FVector& InDeathImpulse)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetDeathImpulse(InDeathImpulse);
+	}
+}
+
+void UOWAbilitySystemLibrary::SetKnockbackForce(FGameplayEffectContextHandle& EffectContextHandle, const FVector& InKnockbackForce)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetKnockbackForce(InKnockbackForce);
+	}
+}
+
+void UOWAbilitySystemLibrary::SetIsRadialDamage(FGameplayEffectContextHandle& EffectContextHandle, bool bInIsRadialDamage)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetIsRadialDamage(bInIsRadialDamage); 
+	}
+}
+
+void UOWAbilitySystemLibrary::SetRadialDamageRadius(FGameplayEffectContextHandle& EffectContextHandle, float InRadius)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetRadialDamageRadius(InRadius); 
+	}
+}
+
+void UOWAbilitySystemLibrary::SetRadialDamageOrigin(FGameplayEffectContextHandle& EffectContextHandle, const FVector& InRadialDamageOrigin)
+{
+	if (FOWGameplayEffectContext* OWEffectContext = static_cast<FOWGameplayEffectContext*>(EffectContextHandle.Get()))
+	{
+		OWEffectContext->SetRadialDamageOrigin(InRadialDamageOrigin); 
+	}
+}
+
+/* Gameplay Mechanics */
+FGameplayEffectContextHandle UOWAbilitySystemLibrary::ApplyDamageEffect(const FDamageEffectParams& DamageEffectParams)
+{
+	const FOWGameplayTags& GameplayTags = FOWGameplayTags::Get(); 
+	const AActor* SourceAvatarActor = DamageEffectParams.SourceAbilitySystemComponent->GetAvatarActor(); 
+
+	FGameplayEffectContextHandle EffectContextHandle = DamageEffectParams.SourceAbilitySystemComponent->MakeEffectContext(); 
+	EffectContextHandle.AddSourceObject(SourceAvatarActor); 
+	SetDeathImpulse(EffectContextHandle, DamageEffectParams.DeathImpulse); 
+	SetKnockbackForce(EffectContextHandle, DamageEffectParams.KnockbackForce); 
+
+	SetIsRadialDamage(EffectContextHandle, DamageEffectParams.bIsRadialDamage); 
+	SetRadialDamageRadius(EffectContextHandle, DamageEffectParams.RadialDamageRadius); 
+	SetRadialDamageOrigin(EffectContextHandle, DamageEffectParams.RadialDamageOrigin); 
+
+	const FGameplayEffectSpecHandle SpecHandle = DamageEffectParams.SourceAbilitySystemComponent->MakeOutgoingSpec(
+		DamageEffectParams.DamageGamePlayEffectClass,
+		DamageEffectParams.AbilityLevel,
+		EffectContextHandle); 
+
+	UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, DamageEffectParams.DamageType, DamageEffectParams.BaseDamage); 
+	UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Debuff_Chance, DamageEffectParams.DebuffChance); 
+	UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Debuff_Damage, DamageEffectParams.DebuffDamage); 
+	UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Debuff_Duration, DamageEffectParams.DebuffDuration); 
+	UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Debuff_Frequency, DamageEffectParams.DebuffFrequency); 
+
+	DamageEffectParams.TargetAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data); 
+
+	return EffectContextHandle; 
+}
+
+void UOWAbilitySystemLibrary::SetKnockbackDirection(FDamageEffectParams& DamageEffectParams, FVector KnockbackDirection, float Magnitude)
+{
+	KnockbackDirection.Normalize(); 
+	if (Magnitude == 0.f)
+	{
+		DamageEffectParams.KnockbackForce = KnockbackDirection * DamageEffectParams.KnockbackForceMagnitude; 
+	}
+	else
+	{
+		DamageEffectParams.KnockbackForce = KnockbackDirection * Magnitude; 
+	}
+}
+
+void UOWAbilitySystemLibrary::SetDeathImpulseDirection(FDamageEffectParams& DamageEffectParams, FVector ImpulseDirection, float Magnitude)
+{
+	ImpulseDirection.Normalize();
+	if (Magnitude == 0.f)
+	{
+		DamageEffectParams.DeathImpulse = ImpulseDirection * DamageEffectParams.DeathImpulseMagnitude;
+	}
+	else
+	{
+		DamageEffectParams.DeathImpulse = ImpulseDirection * Magnitude;
+	}
+}
+
+void UOWAbilitySystemLibrary::SetTargetEffectParamsASC(FDamageEffectParams& DamageEffectParams, UAbilitySystemComponent* InAbilitySystemComponent)
+{
+	DamageEffectParams.TargetAbilitySystemComponent = InAbilitySystemComponent; 
 }
--- Source/FPSTemplate/Public/AbilitySystem/OWAbilitySystemLibrary.h
+++ Source/FPSTemplate/Public/AbilitySystem/OWAbilitySystemLibrary.h
@@ -10,6 +10,7 @@ struct FWidgetControllerParams;
 class AOWHUD; 
 class UOverlayWidgetController; 
 class UHeroInfo; 
+struct FDamageEffectParams; 
 
 /**
  * 
@@ -45,4 +46,91 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|HeroInfoDefaults")
 	static EHeroName GetHeroName(const UObject* WorldContextObject);
+
+	/*
+	 * Effect Context Getter
+	 */
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static bool IsCriticalHit(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static bool IsSuccessfulDebuff(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static float GetDebuffDamage(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static float GetDebuffDuration(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static float GetDebuffFrequency(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static FGameplayTag GetDamageType(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static FVector GetDeathImpulse(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static FVector GetKnockbackForce(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static bool bIsRadialDamage(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static float GetRadialDamageRadius(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static FVector GetRadialDamageOrigin(const FGameplayEffectContextHandle& EffectContextHandle); 
+
+	/* 
+	 * Effect Context Setter
+	 */
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static void SetIsCriticalHit(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, bool bInIsCriticalHit);
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static void SetIsSuccessfulDebuff(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, bool bInIsSuccessfulDebuff);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetDebuffDamage(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, float InDebuffDamage);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetDebuffDuration(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, float InDebuffDuration);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetDebuffFrequency(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, float InDebuffFrequency);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetDamageType(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, const FGameplayTag& InDamageType);
+
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayEffects")
+	static void SetDeathImpulse(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, const FVector& InDeathImpulse);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetKnockbackForce(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, const FVector& InKnockbackForce);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetIsRadialDamage(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, bool bInIsRadialDamage);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetRadialDamageRadius(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, float InRadius);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetRadialDamageOrigin(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, const FVector& InRadialDamageOrigin);
+
+	/*
+	 * Gameplay Mechanics 
+	 */
+	UFUNCTION(BlueprintCallable, Category = "OWAbilitySystemLibrary|GameplayMechanics")
+	static FGameplayEffectContextHandle ApplyDamageEffect(const FDamageEffectParams& DamageEffectParams); 
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|DamageEffect")
+	static void SetKnockbackDirection(UPARAM(ref)FDamageEffectParams& DamageEffectParams, FVector KnockbackDirection, float Magnitude = 0.f);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|DamageEffect")
+	static void SetDeathImpulseDirection(UPARAM(ref)FDamageEffectParams& DamageEffectParams, FVector ImpulseDirection, float Magnitude = 0.f);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|DamageEffect")
+	static void SetTargetEffectParamsASC(UPARAM(ref)FDamageEffectParams& DamageEffectParams, UAbilitySystemComponent* InAbilitySystemComponent);
 };
