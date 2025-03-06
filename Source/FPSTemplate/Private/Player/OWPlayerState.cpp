3333c366dc7c3910e97853b5e9f52f4d39966267
conple21 <conple21@gmail.com>
2025-03-06 12:58:53 +0900

    0306 (4) Update Player State to process Player's LevelUp
    - Declare Delegates to Broadcast Stat Value
    - Set Stat Value, Data Asset, and Delegates as Member Variables
    - Implement Getter and Setter for Stat Value
    - Implement Related RepNotify Function

--- Source/FPSTemplate/Private/Player/OWPlayerState.cpp
+++ Source/FPSTemplate/Private/Player/OWPlayerState.cpp
@@ -39,4 +39,78 @@ void AOWPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLi
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps); 
 	DOREPLIFETIME(AOWPlayerState, SelectedHeroName);
+	DOREPLIFETIME(AOWPlayerState, Level); 
+	DOREPLIFETIME(AOWPlayerState, XP); 
+	DOREPLIFETIME(AOWPlayerState, AttributePoints); 
+	DOREPLIFETIME(AOWPlayerState, SpellPoints); 
 }
+
+void AOWPlayerState::AddToXP(int32 InXP)
+{
+	XP += InXP; 
+	OnXPChangedDelegate.Broadcast(XP); 
+}
+
+void AOWPlayerState::AddToLevel(int32 InLevel)
+{
+	Level += InLevel; 
+	OnLevelChangedDelegate.Broadcast(Level, true); 
+}
+
+void AOWPlayerState::AddToAttributePoints(int32 InAttributePoints)
+{
+	AttributePoints += InAttributePoints; 
+	OnAttributePointsChangedDelegate.Broadcast(AttributePoints); 
+}
+
+void AOWPlayerState::AddToSpellPoints(int32 InSpellPoints)
+{
+	SpellPoints += InSpellPoints; 
+	OnSpellPointsChangedDelegate.Broadcast(SpellPoints); 
+}
+
+void AOWPlayerState::SetXP(int32 InXP)
+{
+	XP = InXP; 
+	OnXPChangedDelegate.Broadcast(XP); 
+}
+
+void AOWPlayerState::SetLevel(int32 InLevel)
+{
+	Level = InLevel; 
+	OnLevelChangedDelegate.Broadcast(Level, false); 
+}
+
+void AOWPlayerState::SetAttributePoints(int32 InAttributePoints)
+{
+	AttributePoints = InAttributePoints;
+	OnAttributePointsChangedDelegate.Broadcast(AttributePoints); 
+}
+
+void AOWPlayerState::SetSpellPoints(int32 InSpellPoints)
+{
+	SpellPoints = InSpellPoints; 
+	OnSpellPointsChangedDelegate.Broadcast(SpellPoints); 
+}
+
+
+void AOWPlayerState::OnRep_Level(int32 OldLevel)
+{
+	OnLevelChangedDelegate.Broadcast(Level, true); 
+}
+
+void AOWPlayerState::OnRep_XP(int32 OldXP)
+{
+	OnXPChangedDelegate.Broadcast(XP); 
+}
+
+void AOWPlayerState::OnRep_AttributePoints(int32 OldAttributePoints)
+{
+	OnAttributePointsChangedDelegate.Broadcast(AttributePoints); 
+}
+
+void AOWPlayerState::OnRep_SpellPoints(int32 OldSpellPoints)
+{
+	OnSpellPointsChangedDelegate.Broadcast(SpellPoints); 
+}
+
--- Source/FPSTemplate/Public/Player/OWPlayerState.h
+++ Source/FPSTemplate/Public/Player/OWPlayerState.h
@@ -6,10 +6,15 @@
 #include "Player/DS_MatchPlayerState.h"
 #include "AbilitySystemInterface.h"
 #include "AbilitySystem/Data/HeroInfo.h"
+#include "AbilitySystem/Data/LevelUpInfo.h"
 #include "OWPlayerState.generated.h"
 
 class UAbilitySystemComponent; 
 class UAttributeSet; 
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStateChanged, int32 /* Stat Value */)
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnLevelChanged, int32 /*Stat Value*/, bool /*bLevelUp*/)
+
 /**
  * 
  */
@@ -24,6 +29,7 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet;  }
 
+	/* Hero Info */
 	UPROPERTY(ReplicatedUsing = OnRep_SelectedHeroName, BlueprintReadOnly)
 	EHeroName SelectedHeroName;
 
@@ -32,6 +38,32 @@ public:
 
 	UFUNCTION()
 	void OnRep_SelectedHeroName();
+	/* End Hero Info */
+
+	/* LevelUp Info */
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<ULevelUpInfo> LevelUpInfo; 
+
+	FOnPlayerStateChanged OnXPChangedDelegate;
+	FOnLevelChanged OnLevelChangedDelegate; 
+	FOnPlayerStateChanged OnAttributePointsChangedDelegate; 
+	FOnPlayerStateChanged OnSpellPointsChangedDelegate; 
+
+	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+	FORCEINLINE int32 GetXP() const { return XP; }
+	FORCEINLINE int32 GetAttributePoints() const { return AttributePoints; }
+	FORCEINLINE int32 GetSpellPoints() const { return SpellPoints; }
+
+	void AddToXP(int32 InXP); 
+	void AddToLevel(int32 InLevel); 
+	void AddToAttributePoints(int32 InAttributePoints); 
+	void AddToSpellPoints(int32 InSpellPoints); 
+
+	void SetXP(int32 InXP); 
+	void SetLevel(int32 InLevel); 
+	void SetAttributePoints(int32 InAttributePoints); 
+	void SetSpellPoints(int32 InSpellPoints); 
+	/* End LevelUp Info */
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 protected:
@@ -40,4 +72,29 @@ protected:
 
 	UPROPERTY()
 	TObjectPtr<UAttributeSet> AttributeSet; 
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Level)
+	int32 Level = 1; 
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_XP)
+	int32 XP = 0; 
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_AttributePoints)
+	int32 AttributePoints = 0; 
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing =  OnRep_SpellPoints)
+	int32 SpellPoints = 0; 
+
+private:
+	UFUNCTION()
+	void OnRep_Level(int32 OldLevel); 
+
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP); 
+
+	UFUNCTION()
+	void OnRep_AttributePoints(int32 OldAttributePoints); 
+
+	UFUNCTION()
+	void OnRep_SpellPoints(int32 OldSpellPoints); 
 };
