07dd0b00638908126d9cf05631a53f5634a74bac
conple21 <conple21@gmail.com>
2025-03-07 14:17:31 +0900

    0307 (2) Initialize Training Omnic
    - Setup an XP Curve Table for Training Omnic
    - Setup Training Omnic's Startup Ability which is based on Damage Gameplay Ability
    - Setup Training Omnic's Vital Attributes which are based on Gameplay Effect

---
+++ Source/FPSTemplate/Public/AbilitySystem/Abilities/Omnic/TrainingOmnic/TrainingOmnicAttack.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/OWDamageGameplayAbility.h"
+#include "TrainingOmnicAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API UTrainingOmnicAttack : public UOWDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
