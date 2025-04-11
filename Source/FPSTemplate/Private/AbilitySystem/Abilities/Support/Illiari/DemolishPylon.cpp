ff289eab1daa4cecea757005ba9aebaa5ebb6d68
conple21 <conple21@gmail.com>
2025-04-11 17:41:34 +0900

    0411 (2) Update Illiari's Gameplay Ability - Demolish Pylon
    - CanActivateAbility()
      > Check if Owner Character and Pylon are Spawned
    - ActivateAbility()
      > Destroy Pylon
      > Add Pylon Destroyed Widget to Viewport

--- Source/FPSTemplate/Private/AbilitySystem/Abilities/Support/Illiari/DemolishPylon.cpp
+++ Source/FPSTemplate/Private/AbilitySystem/Abilities/Support/Illiari/DemolishPylon.cpp
@@ -2,7 +2,52 @@
 
 
 #include "AbilitySystem/Abilities/Support/Illiari/DemolishPylon.h"
+#include "GameFramework/Character.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Actor/HealingSunStone.h"
+#include "UI/Widget/OWUserWidget.h"
 
+bool UDemolishPylon::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
+{
+	ACharacter* OwnerCharacter = Cast<ACharacter>(GetAvatarActorFromActorInfo());
 
+	return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags); 
+}
 
+void UDemolishPylon::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData); 
 
+	// Set Owner Character
+	ACharacter* OwnerCharacter = Cast<ACharacter>(GetAvatarActorFromActorInfo()); 
+	// Set Player Controller
+	APlayerController* PlayerController = Cast<APlayerController>(OwnerCharacter->GetController()); 
+
+	// Project World Location to Widget Position 
+	FVector2D ScreenPosition; 
+	UWidgetLayoutLibrary::ProjectWorldLocationToWidgetPosition(PlayerController, Pylon->GetActorLocation(), ScreenPosition, false);
+
+	// Demolish Pylon 
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+	if (bIsServer)
+	{
+		Pylon->Destroy();
+	}
+
+	// Create Pylon Destroyed Widget
+	if (IsLocallyControlled())
+	{
+		PylonDestroyedWidget = CreateWidget<UOWUserWidget>(this, PylonDestroyedWidgetClass); 
+		PylonDestroyedWidget->AddToViewport(); 
+		PylonDestroyedWidget->SetPositionInViewport(ScreenPosition, false); 
+		GetWorld()->GetTimerManager().SetTimer(RemoveWidgetTimerHandle, this, &UDemolishPylon::RemoveWidget, 2.f, false); 
+	}
+}
+
+void UDemolishPylon::RemoveWidget()
+{
+	if (PylonDestroyedWidget)
+	{
+		PylonDestroyedWidget->RemoveFromParent(); 
+	}
+}
--- Source/FPSTemplate/Public/AbilitySystem/Abilities/Support/Illiari/DemolishPylon.h
+++ Source/FPSTemplate/Public/AbilitySystem/Abilities/Support/Illiari/DemolishPylon.h
@@ -6,6 +6,9 @@
 #include "AbilitySystem/Abilities/OWGameplayAbility.h"
 #include "DemolishPylon.generated.h"
 
+class AHealingSunStone; 
+class UOWUserWidget;
+
 /**
  * 
  */
@@ -14,7 +17,31 @@ class FPSTEMPLATE_API UDemolishPylon : public UOWGameplayAbility
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+public:
+	UPROPERTY()
+	TWeakObjectPtr<AHealingSunStone> Pylon; 
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UOWUserWidget> PylonDestroyedWidgetClass; 
+
+	UPROPERTY()
+	TObjectPtr<UOWUserWidget> PylonDestroyedWidget; 
+
+	FTimerHandle RemoveWidgetTimerHandle; 
+
+	UFUNCTION()
+	void RemoveWidget(); 
+protected:
+	virtual bool CanActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayTagContainer* SourceTags,
+		const FGameplayTagContainer* TargetTags,
+		OUT FGameplayTagContainer* OptionalRelevantTags) const override; 
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override; 
 };
