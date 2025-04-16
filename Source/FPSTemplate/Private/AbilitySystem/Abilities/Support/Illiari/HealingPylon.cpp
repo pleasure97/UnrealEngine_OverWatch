d57e94962bbf65d9bf65607b18361e6884bf9d85
conple21 <conple21@gmail.com>
2025-04-16 15:44:59 +0900

    0416 (1) Update Gameplay Ability - Healing Pylon
    - Add Member Variables
      > Healing Sun Stone, Spawn Relative Location, Throwing Speed, Widget, CooldownTime, and TimerHandle...
    - ActivateAbility()
      > Spawn Healing Sun Stone Deferred
      > Bind Destroyed and Gameplay Attribute Value Change Delegates to Callback Functions
      > Calculate Throw Velocity (Get Normal of Pylon Relative Location)
    - OnHealingSunStoneDestroyed()
      > Get Health Gameplay Attribute Value
      > If Health is 0 (Clamped to 0), Apply Damaged Cooldown Time, else General Cooldown Time
      > End Ability
    - OnHealingSunStoneDamaged()
      > Check Attribute's New Value is Less than Old Value
      > if Health is 0, Destroy Healing Sun Stone, Add Destroyed Widget, and Delete the Widget After 2 Seocnds
      > Calculate Healing Sun Stone Damaged Elapsed Time
    - ApplyDynamicCooldown()
      > Make Gameplay Effect Spec for Cooldown Gameplay Effect
      > Set By Caller to Dynamically apply Cooldown
      > Apply Gameplay Effect Spec To Owner
    - RemoveWidget()

--- Source/FPSTemplate/Private/AbilitySystem/Abilities/Support/Illiari/HealingPylon.cpp
+++ Source/FPSTemplate/Private/AbilitySystem/Abilities/Support/Illiari/HealingPylon.cpp
@@ -2,14 +2,132 @@
 
 
 #include "AbilitySystem/Abilities/Support/Illiari/HealingPylon.h"
+#include "Actor/HealingSunStone.h"
+#include "OWGameplayTags.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/OWAttributeSet.h"
+#include "GameFramework/Character.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "UI/Widget/OWUserWidget.h"
 
 void UHealingPylon::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData); 
 
-	CommitAbilityCost(Handle, ActorInfo, ActivationInfo); 
+	const FVector ActorLocation = ActorInfo->AvatarActor->GetRootComponent()->GetComponentLocation(); 
 
-	CommitAbilityCooldown(Handle, ActorInfo, ActivationInfo, true); 
+	// Spawn Healing SunStone
+	FTransform SpawnTransform; 
+	SpawnTransform.SetLocation(ActorLocation + PylonRelativeLocation); 
+	HealingSunStone = GetWorld()->SpawnActorDeferred<AHealingSunStone>(
+		HealingSunStoneClass,
+		SpawnTransform,
+		GetOwningActorFromActorInfo(),
+		CurrentActorInfo->PlayerController->GetPawn(),
+		ESpawnActorCollisionHandlingMethod::AlwaysSpawn); 
 
-	EndAbility(Handle, ActorInfo, ActivationInfo, true, false); 
+	// Bind OnDestroyed
+	HealingSunStone->OnDestroyed.AddDynamic(this, &UHealingPylon::OnHealingSunStoneDestroyed);
+
+	// Bind OnDamaged 
+	UAbilitySystemComponent* HealingSunStoneASC = HealingSunStone->GetAbilitySystemComponent();
+	if (!HealingSunStoneASC) return;
+
+	HealingSunStoneASC->GetGameplayAttributeValueChangeDelegate(
+		UOWAttributeSet::GetHealthAttribute()).AddUObject(this, &UHealingPylon::OnHealingSunStoneDamaged);
+
+	HealingSunStoneASC->GetGameplayAttributeValueChangeDelegate(
+		UOWAttributeSet::GetShieldAttribute()).AddUObject(this, &UHealingPylon::OnHealingSunStoneDamaged);
+
+	HealingSunStone->SetOwner(GetAvatarActorFromActorInfo()); 
+
+	HealingSunStone->FinishSpawning(SpawnTransform); 
+
+	// Throw Healing SunStone
+	FVector ThrowVelocity = PylonRelativeLocation.GetSafeNormal() * PylonThrowingSpeed;
+	HealingSunStone->Throw(ThrowVelocity);
 }
+
+void UHealingPylon::OnHealingSunStoneDestroyed(AActor* DestroyedActor)
+{
+	UAbilitySystemComponent* HealingSunStoneASC = HealingSunStone->GetAbilitySystemComponent();
+	if (!HealingSunStoneASC) return; 
+
+	bool bHealthAttributeFound = false; 
+	float Health = HealingSunStoneASC->GetGameplayAttributeValue(UOWAttributeSet::GetHealthAttribute(), bHealthAttributeFound); 
+	if (!bHealthAttributeFound) return; 
+
+	if (Health == 0.f || bSunStoneDamaged)
+	{
+		ApplyDynamicCooldown(DamagedCooldownTime); 
+	}
+	else
+	{
+		ApplyDynamicCooldown(GeneralCooldownTime); 
+	}
+
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UHealingPylon::OnHealingSunStoneDamaged(const FOnAttributeChangeData& Data)
+{
+	if (Data.NewValue >= Data.OldValue) return; 
+
+	if (Data.Attribute == UOWAttributeSet::GetHealthAttribute())
+	{
+		if (Data.NewValue == 0.f)
+		{
+			if (GetAvatarActorFromActorInfo()->HasAuthority())
+			{
+				HealingSunStone->Destroy();
+			}
+
+			if (IsLocallyControlled())
+			{
+				// Set Owner Character
+				ACharacter* OwnerCharacter = Cast<ACharacter>(GetAvatarActorFromActorInfo());
+				// Set Player Controller
+				APlayerController* PlayerController = Cast<APlayerController>(OwnerCharacter->GetController());
+
+				// Project World Location to Widget Position 
+				FVector2D ScreenPosition;
+				UWidgetLayoutLibrary::ProjectWorldLocationToWidgetPosition(PlayerController, HealingSunStone->GetActorLocation(), ScreenPosition, false);
+
+				PylonDestroyedWidget = CreateWidget<UOWUserWidget>(PlayerController, PylonDestroyedWidgetClass);
+				PylonDestroyedWidget->AddToViewport();
+				PylonDestroyedWidget->SetPositionInViewport(ScreenPosition, false);
+				GetWorld()->GetTimerManager().SetTimer(RemoveWidgetTimerHandle, this, &UHealingPylon::RemoveWidget, 2.f, false);
+			}
+		}
+	}
+
+	if (DamageCheckTimerHandle.IsValid())
+	{
+		GetWorld()->GetTimerManager().ClearTimer(DamageCheckTimerHandle); 
+		bSunStoneDamaged = true; 
+	}
+	GetWorld()->GetTimerManager().SetTimer(DamageCheckTimerHandle, this, &UHealingPylon::UpdateSunStoneDamagedState, 3.f, false);
+}
+
+void UHealingPylon::UpdateSunStoneDamagedState()
+{
+	bSunStoneDamaged = false; 
+}
+
+void UHealingPylon::ApplyDynamicCooldown(float CooldownTime)
+{
+	FGameplayEffectSpecHandle CooldownSpecHandle = MakeOutgoingGameplayEffectSpec(CooldownGameplayEffectClass, GetAbilityLevel());
+
+	CooldownSpecHandle.Data->SetSetByCallerMagnitude(FOWGameplayTags::Get().Data_Cooldown,CooldownTime); 
+
+	ApplyGameplayEffectSpecToOwner(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, CooldownSpecHandle);
+}
+
+void UHealingPylon::RemoveWidget()
+{
+	if (PylonDestroyedWidget)
+	{
+		PylonDestroyedWidget->RemoveFromParent();
+	}
+}
+
--- Source/FPSTemplate/Public/AbilitySystem/Abilities/Support/Illiari/HealingPylon.h
+++ Source/FPSTemplate/Public/AbilitySystem/Abilities/Support/Illiari/HealingPylon.h
@@ -3,20 +3,55 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AbilitySystem/Abilities/OWDamageGameplayAbility.h"
+#include "AbilitySystem/Abilities/OWGameplayAbility.h"
 #include "HealingPylon.generated.h"
 
+class AHealingSunStone; 
+class UDemolishPylon; 
+class UOWUserWidget; 
 /**
  * 
  */
 UCLASS()
-class FPSTEMPLATE_API UHealingPylon : public UOWDamageGameplayAbility
+class FPSTEMPLATE_API UHealingPylon : public UOWGameplayAbility
 {
 	GENERATED_BODY()
 
+	friend class UDemolishPylon; 
 public:
+	/* Healing Sun Stone Spawn */
+	UPROPERTY()
+	TObjectPtr<AHealingSunStone> HealingSunStone; 
 
-	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf <AHealingSunStone> HealingSunStoneClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FVector PylonRelativeLocation = FVector(56.f, 2.f, 56.f); 
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float PylonThrowingSpeed = 800.f; 
+
+	/* UI */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UOWUserWidget> PylonDestroyedWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UOWUserWidget> PylonDestroyedWidget;
+
+	FTimerHandle RemoveWidgetTimerHandle;
+	FTimerHandle DamageCheckTimerHandle; 
+
+	UFUNCTION()
+	void RemoveWidget();
+
+	/* Cooldown */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float GeneralCooldownTime = 6.f; 
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float DamagedCooldownTime = 14.f;
+
 protected:
 	virtual void ActivateAbility(
 		const FGameplayAbilitySpecHandle Handle, 
@@ -24,7 +59,15 @@ protected:
 		const FGameplayAbilityActivationInfo ActivationInfo, 
 		const FGameplayEventData* TriggerEventData) override;
 
-	
-	
-	
+	UFUNCTION()
+	void OnHealingSunStoneDestroyed(AActor* DestroyedActor); 
+
+	void OnHealingSunStoneDamaged(const FOnAttributeChangeData& Data);
+
+	UFUNCTION()
+	void UpdateSunStoneDamagedState(); 
+
+	void ApplyDynamicCooldown(float CooldownTime); 
+
+	bool bSunStoneDamaged = false;
 };
