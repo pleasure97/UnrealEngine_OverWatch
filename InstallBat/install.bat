5640380a4215e69d7df6eff183b70f14e1dfcd95
conple21 <conple21@gmail.com>
2025-04-20 16:20:06 +0900

    0420 (2) Update AWS install.bat file

--- InstallBat/install.bat
+++ InstallBat/install.bat
@@ -1,1 +1,1 @@
-Engine\Extras\Redist\en-us\UEPrereqSetup_x64.exe /install /quiet /norestart /log C:\game\vcredist_2013_x64.log
+Engine\Extras\Redist\en-us\UEPrereqSetup_x64.exe /install /quiet /norestart /log C:\UnrealProjects\vcredist_2013_x64.log
