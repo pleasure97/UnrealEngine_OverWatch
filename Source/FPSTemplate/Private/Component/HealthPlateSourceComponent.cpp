fae8819d9d3db7952860d37e24fa558b6affca67
conple21 <conple21@gmail.com>
2025-05-18 16:19:01 +0900

    0518 (4) Implement Health Plate Source Component
    - BeginPlay()
      > Get Gameplay Message Subsystem
      >  Add Health Plate For Self Pawn
      > Listen for HealthPlateManagerComponent
    - OnHealthPlateDiscovered()
      > Get Health Plate Manager Component from Payload
      > Register Character to Health Plate Source
    - EndPlay()
      > Broadcast to Remove Health Plate Source

--- Source/FPSTemplate/Private/Component/HealthPlateSourceComponent.cpp
+++ Source/FPSTemplate/Private/Component/HealthPlateSourceComponent.cpp
@@ -1,35 +1,65 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Component/HealthPlateSourceComponent.h"
+#include "GameFramework/GameplayMessageSubsystem.h"
+#include "GameFramework/AsyncAction_ListenForGameplayMessage.h"
+#include "OWGameplayTags.h"
+#include "Message/OWMessageTypes.h"
+#include "Component/HealthPlateManagerComponent.h"
 
-
-// Sets default values for this component's properties
 UHealthPlateSourceComponent::UHealthPlateSourceComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
-
-// Called when the game starts
 void UHealthPlateSourceComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	// Get Gameplay Message Subsystem
+	UGameplayMessageSubsystem& GameplayMessageSubsystem = UGameplayMessageSubsystem::Get(this); 
+
+	/* 1. Add Health Plate For Self */
+	// Set Health Plate Add Tag To Broadcast
+	FGameplayTag HealthPlateAddTag = FOWGameplayTags::Get().Gameplay_Message_HealthPlate_Add; 
+
+	// Construct Health Plate Info to Broadcast
+	APawn* OwnerPawn = Cast<APawn>(GetOwner()); 
+	FHealthPlateInfo HealthPlateInfo = FHealthPlateInfo(OwnerPawn); 
+
+	// Broadcast Health Plate Info 
+	GameplayMessageSubsystem.BroadcastMessage(HealthPlateAddTag, HealthPlateInfo);
+
+	/* 2. Listen for HealthPlateManagerComponent */
+
+	// Set Health Plate Discover Tag to Broadcast
+	FGameplayTag HealthPlateDiscoverTag = FOWGameplayTags::Get().Gameplay_Message_HealthPlate_Discover;
+
+	FGameplayMessageListenerHandle ListenerHandle = 
+		GameplayMessageSubsystem.RegisterListener<FHealthPlateManagerInfo>(HealthPlateDiscoverTag, this, &UHealthPlateSourceComponent::OnHealthPlateDiscovered);
 }
 
-
-// Called every frame
-void UHealthPlateSourceComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UHealthPlateSourceComponent::OnHealthPlateDiscovered(FGameplayTag Channel, const FHealthPlateManagerInfo& Payload)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+	UHealthPlateManagerComponent* HealthPlateManager = Payload.HealthPlateManagerComponent;
+	APawn* OwnerPawn = Cast<APawn>(GetOwner()); 
+	HealthPlateManager->RegisterHealthPlateSource(OwnerPawn); 
 }
 
+
+void UHealthPlateSourceComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	// Get Gameplay Message Subsystem
+	UGameplayMessageSubsystem& GameplayMessageSubsystem = UGameplayMessageSubsystem::Get(this);
+
+	// Set Health Plate Remove Tag To Broadcast
+	FGameplayTag HealthPlateRemoveTag = FOWGameplayTags::Get().Gameplay_Message_HealthPlate_Remove;
+
+	// Construct Health Plate Info to Broadcast
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	FHealthPlateInfo HealthPlateInfo = FHealthPlateInfo(OwnerPawn);
+
+	// Broadcast Health Plate Info 
+	GameplayMessageSubsystem.BroadcastMessage(HealthPlateRemoveTag, HealthPlateInfo);
+
+	Super::EndPlay(EndPlayReason); 
+}
--- Source/FPSTemplate/Public/Component/HealthPlateSourceComponent.h
+++ Source/FPSTemplate/Public/Component/HealthPlateSourceComponent.h
@@ -6,6 +6,8 @@
 #include "Components/ActorComponent.h"
 #include "HealthPlateSourceComponent.generated.h"
 
+struct FGameplayTag; 
+struct FHealthPlateManagerInfo;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class FPSTEMPLATE_API UHealthPlateSourceComponent : public UActorComponent
@@ -13,17 +15,12 @@ class FPSTEMPLATE_API UHealthPlateSourceComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UHealthPlateSourceComponent();
 
+	UFUNCTION()
+	void OnHealthPlateDiscovered(FGameplayTag Channel, const FHealthPlateManagerInfo& Payload);
+
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
-	
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override; 
 };
