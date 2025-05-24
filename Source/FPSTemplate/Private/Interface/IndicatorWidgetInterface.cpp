ea14d3351ca929bba30296d33c8fae92d282afa8
conple21 <conple21@gmail.com>
2025-05-24 15:04:10 +0900

    0524 (2) Declare Indicator Widget Interface

---
+++ Source/FPSTemplate/Private/Interface/IndicatorWidgetInterface.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface/IndicatorWidgetInterface.h"
+
+
+// Add default functionality here for any IIndicatorWidgetInterface functions that are not pure virtual.
