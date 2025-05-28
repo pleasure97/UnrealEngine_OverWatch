3470584f8a1d38ba76f07452db404ccd7c06a34f
conple21 <conple21@gmail.com>
2025-05-28 16:32:12 +0900

    0528 (5) Update Hero Selection Widget
    - NativeConstruct()
      > Initialize Button Design
      > Bind Each Button Delegate to Callback
    - NativeDestruct()
      > Unbind Each Button Delegate
    - SetHeroInfo()
      > Get Hero Info Using Custom Ability Systemt Blueprint Library
      > Set Hero Name Text and Hero Portrait
    - Button Callbacks
      > Scale Overlay's Render Transform
      > Broadcast Hero Name

--- Source/FPSTemplate/Private/UI/Widget/HeroSelection/HeroSelection.cpp
+++ Source/FPSTemplate/Private/UI/Widget/HeroSelection/HeroSelection.cpp
@@ -2,7 +2,135 @@
 
 
 #include "UI/Widget/HeroSelection/HeroSelection.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
+#include "Components/Button.h"
+#include "Components/Border.h"
+#include "Components/Overlay.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+#include "AbilitySystem/OWAbilitySystemLibrary.h"
 
+void UHeroSelection::NativeConstruct()
+{
+	if (Button_HeroSelection)
+	{
+		// Button Visual Effect
+		if (Border_HeroSelection)
+		{
+			Border_HeroSelection->SetBrushColor(HeroSelectionButtonColors::Gray);
+		}
+		if (Overlay_HeroSelection)
+		{
+			Overlay_HeroSelection->SetRenderTransformPivot(FVector2D(0.5f, 0.5f));
+		}
+		if (TextBlock_HeroName)
+		{
+			TextBlock_HeroName->SetVisibility(ESlateVisibility::Collapsed);
+		}
 
+		// Bind Each Button Delegate to Callback
+		Button_HeroSelection->OnPressed.AddDynamic(this, &UHeroSelection::OnHeroSelectionButtonPressed);
+		Button_HeroSelection->OnHovered.AddDynamic(this, &UHeroSelection::OnHeroSelectionButtonHovered); 
+		Button_HeroSelection->OnUnhovered.AddDynamic(this, &UHeroSelection::OnHeroSelectionButtonUnhovered); 
+		Button_HeroSelection->OnReleased.AddDynamic(this, &UHeroSelection::OnHeroSelectionButtonReleased); 
+	}
+}
+
+void UHeroSelection::NativeDestruct()
+{
+	if (Button_HeroSelection)
+	{
+		// Unbind Each Button Delegate
+		Button_HeroSelection->OnPressed.RemoveAll(this); 
+		Button_HeroSelection->OnHovered.RemoveAll(this); 
+		Button_HeroSelection->OnUnhovered.RemoveAll(this); 
+		Button_HeroSelection->OnReleased.RemoveAll(this); 
+	}
+
+	Super::NativeDestruct(); 
+}
+
+void UHeroSelection::SetHeroInfo(EHeroName InHeroName)
+{
+	HeroName = InHeroName; 
+
+	// Get Hero Info from Using Custom Ability System Library 
+	FOWHeroInfo OWHeroInfo;
+	UOWAbilitySystemLibrary::GetIndividualHeroInfo(this, HeroName, OWHeroInfo);
+
+	// Set Hero Name Text
+	FText HeroNameText = StaticEnum<EHeroName>()->GetDisplayNameTextByValue((int64)HeroName);
+	if (TextBlock_HeroName)
+	{
+		TextBlock_HeroName->SetText(HeroNameText);
+	}
+
+	// Set Hero Portrait
+	if (Image_HeroPortrait2D)
+	{
+		Image_HeroPortrait2D->SetBrushFromTexture(const_cast<UTexture2D*>(OWHeroInfo.HeroPortrait2D.Get()), true);
+	}
+}
+
+void UHeroSelection::OnHeroSelectionButtonPressed()
+{
+	// Button Visual Effect
+	if (Overlay_HeroSelection)
+	{
+		FWidgetTransform WidgetTransform = Overlay_HeroSelection->RenderTransform;
+		WidgetTransform.Scale = FVector2D(1.1f, 1.1f);
+		Overlay_HeroSelection->SetRenderTransform(WidgetTransform);
+	}
+	
+	if (Border_HeroSelection)
+	{
+		Border_HeroSelection->SetBrushColor(HeroSelectionButtonColors::Orange);
+	}
+
+	// Camera Lens Effect
+
+	// Broadcast Hero Name 
+	OnHeroSelected.Broadcast(HeroName); 
+}
+
+void UHeroSelection::OnHeroSelectionButtonHovered()
+{
+	if (Overlay_HeroSelection)
+	{
+		FWidgetTransform WidgetTransform = Overlay_HeroSelection->RenderTransform;
+		WidgetTransform.Scale = FVector2D(1.1f, 1.1f);
+		Overlay_HeroSelection->SetRenderTransform(WidgetTransform);
+	}
+	
+	if (Border_HeroSelection)
+	{
+		Border_HeroSelection->SetBrushColor(HeroSelectionButtonColors::White);
+	}
+}
+
+void UHeroSelection::OnHeroSelectionButtonUnhovered()
+{
+	if (Overlay_HeroSelection)
+	{
+		FWidgetTransform WidgetTransform = Overlay_HeroSelection->RenderTransform;
+		WidgetTransform.Scale = FVector2D(1.f, 1.f);
+		Overlay_HeroSelection->SetRenderTransform(WidgetTransform);
+	}
+	
+	if (Border_HeroSelection)
+	{
+		Border_HeroSelection->SetBrushColor(HeroSelectionButtonColors::Gray);
+	}
+}
+
+void UHeroSelection::OnHeroSelectionButtonReleased()
+{
+	if (Border_HeroSelection)
+	{
+		Border_HeroSelection->SetBrushColor(HeroSelectionButtonColors::Gray);
+	}
+
+	OnHeroUnselected.Broadcast(); 
+}
 
 
--- Source/FPSTemplate/Public/UI/Widget/HeroSelection/HeroSelection.h
+++ Source/FPSTemplate/Public/UI/Widget/HeroSelection/HeroSelection.h
@@ -4,8 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "UI/Widget/OWUserWidget.h"
+#include "AbilitySystem/Data/HeroInfo.h"
 #include "HeroSelection.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHeroSelected, EHeroName, HeroName); 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnHeroUnselected); 
+
+namespace HeroSelectionButtonColors
+{
+	constexpr FLinearColor Orange(1.f, 0.295461f, 0.f, 1.f);
+	constexpr FLinearColor Gray(0.1f, 0.1f, 0.1f, 1.f);
+	constexpr FLinearColor White(0.9f, 0.9f, 0.9f, 1.f); 
+}
+
+class UOverlay; 
 class UButton; 
 class UImage; 
 class UBorder; 
@@ -21,7 +33,13 @@ class FPSTEMPLATE_API UHeroSelection : public UOWUserWidget
 	
 public:
 	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UOverlay> Overlay_HeroSelection;
+
+	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UButton> Button_HeroSelection; 
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UBorder> Border_HeroSelection; 
 
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UImage> Image_HeroPortrait2D;	
@@ -31,4 +49,32 @@ public:
 
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UTextBlock> TextBlock_HeroName; 
+
+	UPROPERTY(BlueprintAssignable)
+	FOnHeroSelected OnHeroSelected; 
+
+	UPROPERTY(BlueprintAssignable)
+	FOnHeroUnselected OnHeroUnselected;
+
+	void SetHeroInfo(EHeroName InHeroName);
+
+protected:
+	virtual void NativeConstruct() override; 
+	virtual void NativeDestruct() override; 
+
+private:
+	UPROPERTY()
+	EHeroName HeroName;
+
+	UFUNCTION()
+	void OnHeroSelectionButtonPressed(); 
+
+	UFUNCTION()
+	void OnHeroSelectionButtonHovered();
+
+	UFUNCTION()
+	void OnHeroSelectionButtonUnhovered();
+
+	UFUNCTION()
+	void OnHeroSelectionButtonReleased();
 };
