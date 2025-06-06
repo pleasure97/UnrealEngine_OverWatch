0f2f136a5bf8519dcacd7eba9aab427b38039511
conple21 <conple21@gmail.com>
2025-06-06 17:10:08 +0900

    0602 (3) Implement Loading Interface
    - ShouldShowLoadingScreen()
      > Checks Whether the Test Object in the Argument Requires a Loading Screen and Returns the Reason for this.

---
+++ Plugins/LoadingScreen/Source/LoadingScreen/Private/LoadingInterface.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LoadingInterface.h"
+
+
+// Add default functionality here for any ILoadingInterface functions that are not pure virtual.
