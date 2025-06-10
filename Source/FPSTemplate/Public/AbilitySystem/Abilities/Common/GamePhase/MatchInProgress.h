7ae0962cdd617aaf1fefa0342e68bd838def4a3e
conple21 <conple21@gmail.com>
2025-06-10 17:12:08 +0900

    0610 (1) Initialize Game Phase Abilities
    - Change Default Game Phase Ability's Directory
    - Inherit Default Game Phase Ability
      > MatchPreparation
      > MatchInProgress
      > PostMatch
    - Initialize Blueprint Version of Game Phase Abilities

---
+++ Source/FPSTemplate/Public/AbilitySystem/Abilities/Common/GamePhase/MatchInProgress.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/Common/GamePhase/OWGamePhaseAbility.h"
+#include "MatchInProgress.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API UMatchInProgress : public UOWGamePhaseAbility
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
