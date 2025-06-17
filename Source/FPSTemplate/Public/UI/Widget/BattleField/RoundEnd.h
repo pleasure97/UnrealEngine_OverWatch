31662d76e4874f689a15bd1ee3227e153165dbc5
conple21 <conple21@gmail.com>
2025-06-17 15:43:55 +0900

    0617 (1) Initialize RoundEnd Widget
    - Add RoundEnd Image Asset
    - Bind NumRounds, Team1 Score, Team2 Score Text Block Widgets

---
+++ Source/FPSTemplate/Public/UI/Widget/BattleField/RoundEnd.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/OWUserWidget.h"
+#include "RoundEnd.generated.h"
+
+class UTextBlock; 
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API URoundEnd : public UOWUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_NumRounds; 
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_Team1Score; 
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_Team2Score;
+};
