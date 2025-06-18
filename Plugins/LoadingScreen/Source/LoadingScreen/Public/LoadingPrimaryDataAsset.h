6182cec152c57095e2a7679af8cbbfd92fb31bf2
conple21 <conple21@gmail.com>
2025-06-18 16:42:21 +0900

    0618 (2) Implement Loading Primary Data Asset
    - Inherit Primary Data Asset
    - Setup Level to be Loaded
    - Implement Blueprint Version of Loading Primary Data Asset
    - Add Loading Primary Data Asset to Engine's Asset Manager Settings

---
+++ Plugins/LoadingScreen/Source/LoadingScreen/Public/LoadingPrimaryDataAsset.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LoadingPrimaryDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOADINGSCREEN_API ULoadingPrimaryDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftObjectPtr<UWorld> LoadingLevel; 
+
+};
