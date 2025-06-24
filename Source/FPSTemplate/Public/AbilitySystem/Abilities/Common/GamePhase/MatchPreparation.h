444514a08cf333ac5ab36e1e8c49c3f0b5f19e60
conple21 <conple21@gmail.com>
2025-06-24 09:42:11 +0900

    0624 (2) Implement Match Preparation Game Phase
    - Member Variables
      > Match Preparation Time
      > Countdown Duration
      > Remaining Countdown Time
      > Damage Immune Gameplay Effect
      > Next Game Phase Ability
    
    - ActivateAbility()
      > Get Custom Global Ability System
      > Apply Damage Immune Gameplay Effect to All Clients
      > Dealy until the Countdown Heartbeat Starts
    
    - BroadcastCountdownMessage()
      > Get Custom Game State
      > Set Countdown Timer
      > Send a Multicast Reliable Message to Clients about the Countdown
      > Check Net Authority When Countdown is Over
      >  Remove Damage Immune Gameplay Effect from All using Global Ability System
      > Start Match In Progress Game Phase

--- Source/FPSTemplate/Public/AbilitySystem/Abilities/Common/GamePhase/MatchPreparation.h
+++ Source/FPSTemplate/Public/AbilitySystem/Abilities/Common/GamePhase/MatchPreparation.h
@@ -13,8 +13,29 @@ UCLASS()
 class FPSTEMPLATE_API UMatchPreparation : public UOWGamePhaseAbility
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float MatchPreparationTime = 45.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 CountdownDuration = 5;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> DamageImmuneGamelayEffectClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UOWGamePhaseAbility> MatchInProgress; 
+
+protected:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+
+private:
+	int32 RemainingCountdown = 0; 
+
+	void BroadcastCountdownMessage(); 
 };
