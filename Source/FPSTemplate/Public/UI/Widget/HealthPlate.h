4b0832265a7533d53de2afa801509c6bdba601ae
conple21 <conple21@gmail.com>
2025-07-22 21:06:00 +0900

    0722 (2) Update Health Plate Widget Component
    - Health Plate
      > Erase All Codes Related to Indicator Widget
      > Change Health Bar Widget Component Class
      > Save Owner Player State
    
    - HealthPlate::SetPlayerState()
      > Solve the Problem of Client's Player State Being Created Before it is Replicated
    
    - HealthPlate::OnTeamChanged()
      > Bind Team Changed Delegate of Owner Player State

--- Source/FPSTemplate/Public/UI/Widget/HealthPlate.h
+++ Source/FPSTemplate/Public/UI/Widget/HealthPlate.h
@@ -3,80 +3,52 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UI/Widget/OWUserWidget.h"
-#include "Interface/IndicatorWidgetInterface.h"
+#include "Blueprint/UserWidget.h"
 #include "HealthPlate.generated.h"
 
-class UIndicatorController; 
-class UTextBlock; 
-class UHealthBarPool; 
-class UAbilitySystemComponent;
-struct FOnAttributeChangeData; 
+class UPlayerHealthBarPool;
+class UTextBlock;
+class UImage;
 class AOWPlayerState; 
-class AActor; 
-class UImage; 
 
-namespace HealthPlateColors
-{
-	constexpr FLinearColor Blue(0.041667f, 0.6407f, 1.f, 1.f);
-	constexpr FLinearColor Red(0.520833f, 0.005426f, 0.032740f, 1.f);
-	constexpr FLinearColor White(1.f, 1.f, 1.f, 1.f); 
-}
 
 /**
  * 
  */
 UCLASS()
-class FPSTEMPLATE_API UHealthPlate : public UOWUserWidget, public IIndicatorWidgetInterface
+class FPSTEMPLATE_API UHealthPlate : public UUserWidget
 {
 	GENERATED_BODY()
 	
 public:
-	/* Indicator Widget Interface */
-	void BindIndicator_Implementation(UIndicatorController* Indicator);
-	void UnbindIndicator_Implementation(UIndicatorController* Indicator);
-	/* Indicator Widget Interface Ends*/
-
-	/* Callbacks */
-	void OnHealthChanged(const FOnAttributeChangeData& Data); 
-
-	void OnMaxHealthChanged(const FOnAttributeChangeData& Data);
-
-	void OnArmorChanged(const FOnAttributeChangeData& Data);
-
-	void OnMaxArmorChanged(const FOnAttributeChangeData& Data);
-
-	void OnShieldChanged(const FOnAttributeChangeData& Data);
-
-	void OnMaxShieldChanged(const FOnAttributeChangeData& Data);
-
-	void OnOverHealthChanged(const FOnAttributeChangeData& Data);
-
 	/* Widgets */
 	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UHealthBarPool> WBP_HealthBarPool;
+	TObjectPtr<UPlayerHealthBarPool> WBP_PlayerHealthBarPool;
 
-	UPROPERTY(meta=(BindWidget))
+	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UTextBlock> TextBlock_PlayerName; 
 
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UImage> Image_PlayerArrow; 
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FLinearColor EnemyHealthColor = FLinearColor(0.520833f, 0.005426f, 0.032740f, 1.f);
+
+	UFUNCTION(BlueprintCallable)
+	void SetPlayerState(AOWPlayerState* InOWPlayerState);
+
+	void UpdatePlayerHealthBarPool();
+
+	void UpdatePlayerName();
+
+protected:
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+	virtual void NativeDestruct() override; 
+
 private:
-	UFUNCTION()
-	void OnPawnReady(UAbilitySystemComponent* ASC); 
-
-	UFUNCTION()
-	void OnDeath(AActor* DeadActor);
-
-	UFUNCTION()
-	void SetTeamVisuals(bool bTeamSet, int32 TeamID);
-
-	void UpdateVisibility(); 
-
 	UPROPERTY()
-	TObjectPtr<AOWPlayerState> OWPlayerState; 
+	TObjectPtr<AOWPlayerState> OWPlayerState;
 
-	UPROPERTY()
-	TObjectPtr<UIndicatorController> IndicatorController; 
+	UFUNCTION()
+	void OnTeamChanged(UObject* ObjectChangingTeam, int32 OldTeamID, int32 NewTeamID); 
 };
