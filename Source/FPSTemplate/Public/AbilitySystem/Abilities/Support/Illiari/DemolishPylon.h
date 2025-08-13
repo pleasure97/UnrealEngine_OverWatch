bc514535053f6777df100d1112537e543c96d6b8
conple21 <conple21@gmail.com>
2025-08-13 21:04:21 +0900

    0813 (2) Update Illiari's Demolish Pylon Gameplay Ability
    - DemolishPylon::ActivateAbility()
      > Find All Healing Sun Stones in World and Check if Owner of Healing Pylon is Avatar Actor
      > Get Local Controller of Avatar Actor
      > Create Pylon Destroyed Widget, Add it to Player Screen, and Set Widget Position in Viewport Reflecting World Location of Pylon
      > Check if Ability Has Authority and Healing Pylon to Destory is Damaged
      > Then Apply Dynamic Cooldown and Destroy Healing Pylon
      > Remove Widget After 3 Seconds and End Ability

--- Source/FPSTemplate/Public/AbilitySystem/Abilities/Support/Illiari/DemolishPylon.h
+++ Source/FPSTemplate/Public/AbilitySystem/Abilities/Support/Illiari/DemolishPylon.h
@@ -18,30 +18,12 @@ class FPSTEMPLATE_API UDemolishPylon : public UOWGameplayAbility
 	GENERATED_BODY()
 	
 public:
-	UPROPERTY()
-	TWeakObjectPtr<AHealingSunStone> Pylon; 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AHealingSunStone> HealingSunStoneClass;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TSubclassOf<UOWUserWidget> PylonDestroyedWidgetClass; 
+	TSubclassOf<UOWUserWidget> PylonDestroyedWidgetClass;
 
-	UPROPERTY()
-	TObjectPtr<UOWUserWidget> PylonDestroyedWidget; 
-
-	FTimerHandle RemoveWidgetTimerHandle; 
-
-	UFUNCTION()
-	void RemoveWidget(); 
-protected:
-	virtual bool CanActivateAbility(
-		const FGameplayAbilitySpecHandle Handle,
-		const FGameplayAbilityActorInfo* ActorInfo,
-		const FGameplayTagContainer* SourceTags,
-		const FGameplayTagContainer* TargetTags,
-		OUT FGameplayTagContainer* OptionalRelevantTags) const override; 
-
-	virtual void ActivateAbility(
-		const FGameplayAbilitySpecHandle Handle,
-		const FGameplayAbilityActorInfo* ActorInfo,
-		const FGameplayAbilityActivationInfo ActivationInfo,
-		const FGameplayEventData* TriggerEventData) override; 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float TimeToRemoveWidget = 3.f; 
 };
